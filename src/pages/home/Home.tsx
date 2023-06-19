4c3ae4a6554b94e7a43b1953ddfa6365cb92a958
soniamarasa <soniamarasa@hotmail.com>
2023-06-18 21:27:34 -0300

    feat: home dash

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import HeaderDashboard from '../../components/HeaderDashboard/HeaderDashboard';
+import './Home.scss';
+import Charts from '../../components/Charts/Charts';
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="home-container">
+      <HeaderDashboard />
+
+      <div className='body-container grid'>
+        <Charts />
+        <Charts />
+        <Charts />
+        <Charts />
+      </div>
+    </div>
+  );
 };
